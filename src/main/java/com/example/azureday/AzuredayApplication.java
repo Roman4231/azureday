ecb257c920f6c26c605163a79a5e33ee92918ddd
admin <roman.gnativ@botscrew.com>
2018-11-09 08:25:03 +0200

    init commit

---
+++ src/main/java/com/example/azureday/AzuredayApplication.java
@@ -0,0 +1,12 @@
+package com.example.azureday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuredayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzuredayApplication.class, args);
+    }
+}
